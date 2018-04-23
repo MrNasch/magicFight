a680788ecf820dbe046e08685f0fbd84145779e5
MrNasch <nicolaspaschal@hotmil.com>
2018-04-23 20:59:12 +0200

    created magus.swift

---
+++ magicfight/magicfight/magus.swift
@@ -0,0 +1,13 @@
+//creat class magus
+class Magus {
+    var name: String
+    var life = 120
+    var heal = 15
+    init(name: String){
+        self.name = name
+    }
+    
+    func regen() {
+        
+    }
+}
--- magicfight/magicfight/main.swift
+++ magicfight/magicfight/main.swift
@@ -24,19 +24,7 @@ class RandomKill {
 
 
 
-//creat class magus
-class Magus {
-    var name: String
-    var life = 120
-    var heal = 15
-    init(name: String){
-        self.name = name
-    }
-    
-    func regen() {
-        
-    }
-}
+
 
 //creat class colossus
 class Colossus {
