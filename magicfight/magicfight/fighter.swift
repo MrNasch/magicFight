34f4e6758d04636712da547d633645d086bd50a1
MrNasch <nicolaspaschal@hotmil.com>
2018-04-23 20:58:43 +0200

    created fighter.swift

---
+++ magicfight/magicfight/fighter.swift
@@ -0,0 +1,10 @@
+//creat class fighter
+class Fighter {
+    var name = ""
+    var life = 100
+    var attack = 10
+    
+    func strike() {
+        
+    }
+}
--- magicfight/magicfight/main.swift
+++ magicfight/magicfight/main.swift
@@ -22,16 +22,7 @@ class RandomKill {
     
 }
 
-//creat class fighter
-class Fighter {
-    var name = ""
-    var life = 100
-    var attack = 10
-    
-    func strike() {
-        
-    }
-}
+
 
 //creat class magus
 class Magus {
