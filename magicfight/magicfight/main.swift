75ff5e3e46f6fb8570e30dd0ffc5c072c5a75a03
MrNasch <nicolaspaschal@hotmil.com>
2018-05-14 13:57:45 +0200

    created func CreatePersonnageTeam1, CreatePersonnageTeam2, PersonnageInTheGame

--- magicfight/magicfight/main.swift
+++ magicfight/magicfight/main.swift
@@ -9,51 +9,13 @@
 import Foundation
 
 var party: Party = Party()
-for i in 1...2 {
-    print("Create your team player \(i)")
-    let team = Team()
-    print("Choose and name your characters")
-    for j in 1...3 {
-        print("Wich class do you want to play \(j)"
-            + "\n1. Fighter"
-            + "\n2. Magus"
-            + "\n3. Colossus"
-            + "\n4. Dwarf")
-        var personnage: Personnage = Personnage(life: 100)
-        if let choice = readLine() {
-            switch choice {
-            case "1":
-                personnage = Fighter()
-                print("You choose a fighter")
-            case "2":
-                personnage = Magus()
-                print("You choose a magus")
-            case "3":
-                personnage = Colossus()
-                print("You choose a colossus")
-            case "4":
-                personnage = Dwarf()
-                print("You choose a dwarf")
-            default:
-               print("Please select a class")
-            }
-        }
-        print("Name your character")
-        let name = readLine()
-        personnage.name = name!
-        team.personnages.append(personnage)
-    }
-    party.teams.append(team)
-}
-for i in 0...1 {
-    print("Team \(i+1)")
-    let team:Team = party.teams[i]
-    for j in 0...2 {
-        print("Character \(j+1)")
-        let personnage = team.personnages[j]
-        print(personnage.description())
-        print(String(describing: personnage.self))
-    }
-}
-party.ChoosePersonnage()
+var team: Team = Team()
+print(party.newGame)
 
+team.CreatePersonnageTeam1()
+team.CreatePersonnageTeam2()
+
+party.PersonnageInTheGame()
+
+
+party.ChoosePersonnage()
--- magicfight/magicfight/party.swift
+++ magicfight/magicfight/party.swift
@@ -7,14 +7,27 @@ class Party {
     var newGame: String = "Welcome to MagicFight please select 3 characters"
     var endGame: String = "Good job " + "you won"
     
+    func PersonnageInTheGame() {
+        for i in 0...1 {
+            print("Team \(i+1)")
+            let team:Team = party.teams[i]
+            for j in 0...2 {
+                print("Character \(j+1)")
+                let personnage = team.personnages[j]
+                print(String(describing: personnage.self))
+                print(personnage.description())
+            }
+        }
+    }
+    
     func ChoosePersonnage() {
-        print("Please select one personnage for team1")
+        print("Please select one personnage from team1")
         let userChoice = readLine()
         
         if userChoice != nil {
             team1UsedPersonnage = Int(userChoice!)! - 1
         }
-        print("Please select one personnage for team2")
+        print("Please select one personnage from team2")
         let userChoice2 = readLine()
         
         if userChoice2 != nil {
